493561e862ec9cf38f6c7a971970e59fe178699a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-07 11:32:56 +0100

    Added main elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
+import SearchLogo from "./search.svg";
+import MovieCard from "./MovieCard";
 
 function App() {
   return (
     <div className="App">
-         Hello World
+         <h1>MovieIsland <span>📽️</span></h1>
+         <div className='search'>
+           <input type="text" placeholder='Enter your movie...' />
+           <img src={SearchLogo} alt="search logo" />
+         </div>
+         <MovieCard />
     </div>
   );
 }
---
+++ src/MovieCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./MovieCard.css";
+
+const MovieCard = () => {
+    return (
+        <h1>I am a movie</h1>
+    );
+}
+
+export default MovieCard;
