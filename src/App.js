87414f34b8010e2561455bd613731cc41b1e82f8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-07 13:29:45 +0100

    Added API and useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import SearchLogo from "./search.svg";
 import MovieCard from "./MovieCard";
 import Footer from "./Footer";
 
+const API_URL = "http://www.omdbapi.com/?apikey=34a42400&";
+
 function App() {
+
+useEffect(() => {
+  searchMovies("SpiderMan");
+}, []);
+
+const searchMovies = async (title) => {
+  const response = await fetch(`${API_URL}&s=${title}`);
+  const data = await response.json();
+
+  console.log(data.Search);
+}
   return (
     <div className="App">
          <div className="header">
