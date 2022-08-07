e6c804e1f5713d9f2218b97e706ea5d415ebb10a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-07 12:59:43 +0100

    Updated MovieCard
    Added css

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <div className="Footer">
-            Coded by <a href="https://www.lunasmithart.com/" target="_blank" rel="noreferrer">Luna Smith </a>open source on <a href="https://github.com/Lu-Smith/react-movie-app-luna-smith" target="_blank" rel="noreferrer">GitHub</a>
+            Coded by <a href="https://www.lunasmithart.com/" target="_blank" rel="noreferrer">Luna Smith </a>, open source on <a href="https://github.com/Lu-Smith/react-movie-app-luna-smith" target="_blank" rel="noreferrer">GitHub</a>
         </div>
     );
 }
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -4,7 +4,11 @@ import "./MovieCard.css";
 const MovieCard = () => {
     return (
         <div className="MovieCard">
-        <h1>I am a movie</h1>
+        <img src="" alt="movie poster" />
+        <div className="bottom-card">
+        <h4>Movie</h4>
+        <h2>Title</h2>
+        </div>
         </div>
     );
 }
